ad9cf0f7bbfb7e0e7922ee66968204d976fd8ec7
jaseel <jaseeljctcs@gmail.com>
2022-11-21 12:25:47 +0530

    initial

---
+++ src/FindNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FindNumber {
+    public static void main(String[] args) {
+        System.out.println("Please enter number \n 1-Find Largest of 3 numbers \n 2-find smallest of 3 numbers " +
+                "\n 3-check prime or not \n 4-check even or not \n 5-  reverse of your number 6-Exit");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        switch (n){
+
+
+        }
+    }
+}
