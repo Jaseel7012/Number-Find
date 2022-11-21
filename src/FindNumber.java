928538023845335d717d71888a6c08a451d6d728
jaseel <jaseeljctcs@gmail.com>
2022-11-21 12:53:20 +0530

    Small number find

--- src/FindNumber.java
+++ src/FindNumber.java
@@ -12,6 +12,17 @@ public class FindNumber {
             System.out.println(n3+" is large");
         }
     }
+    static void smal(int n1,int n2,int n3){
+        if(n1<=n2 && n1<=n3){
+            System.out.println(n1+" is small");
+        } else if (n2<=n1 && n2<=n3) {
+            System.out.println(n2+" is small");
+
+        }
+        else{
+            System.out.println(n3+" is small");
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Please enter number \n 1-Find Largest of 3 numbers \n 2-find smallest of 3 numbers " +
@@ -23,6 +34,12 @@ public class FindNumber {
                 System.out.println("Enter 3 numbers...\n");
                 Scanner sc1=new Scanner(System.in);
                 large(sc1.nextInt(),sc1.nextInt(),sc1.nextInt());
+                break;
+            case 2:
+                System.out.println("Enter 3 numbers...");
+                Scanner sc2=new Scanner(System.in);
+                smal(sc2.nextInt(),sc2.nextInt(),sc2.nextInt());
+                break;
 
 
         }
